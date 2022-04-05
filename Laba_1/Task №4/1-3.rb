efa407bce88a94704fc658e044f1697b72555d94
VirusTM <71609496+VirusTM@users.noreply.github.com>
2022-04-06 00:24:16 +0300

    Add files via upload

---
+++ Laba_1/Task №4/1-3.rb
@@ -0,0 +1,69 @@
+def one(array)
+
+if array[0]== array[-1]
+  (array.uniq- array[0..0])[0]
+elsif array[0]== array[1]
+  array[-1]
+else
+  array[0]
+end
+end
+
+
+def two(array)
+ puts array.min 
+(array - [array.min]).min
+end
+
+
+R=5.2
+def free(array)
+array.sort_by{ |i| (i-R).abs }[0]
+end
+
+
+
+
+def main
+
+  puts "Выбор задания
+      1) 1 задание
+      2) 2 задание
+      3) 3 задание"
+      my_method = gets.chomp
+  
+
+    puts "Откуда взять массив?
+    1) Ввод с клавиатуры
+    2) Выбор из файла"
+    my_option = gets.chomp
+
+    my_arr = Array.new
+
+    case my_option
+    when "1"
+      puts "Пишете массив через пробел"
+        my_arr = gets.chomp.split.map(&:to_i)
+    when "2"
+        puts "Нужен адрес файла" 
+        my_address = gets.chomp
+        my_str = IO.read(my_address) # C:\Users\VirusTM\Desktop\Task №4\input.txt
+        my_arr = my_str.split.map(&:to_i)
+    else
+        puts "Ошибка в выборе источника массива"
+    end
+
+    case my_method
+    when "1"
+      puts one(my_arr)
+    when "2"
+      puts two(my_arr)
+    when "3"
+      puts free(my_arr)
+  
+  else
+      puts "Ошибка в выборе метода"
+  end
+end
+main
+   
